65c114d041f6710e64d33a2b954f943c88bf5838
Aayush Prasai <gayatriprasai@gmail.com>
2025-05-26 19:36:51 +0545

    update home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,29 @@
 
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-
-function App() {
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { LoginSignup } from './Pages/LoginSignup';
+import { Product } from './Pages/Product';
+import { ShopCategory } from './Pages/ShopCategory';
+import { Shop } from './Pages/Shop';
+import { Cart } from './Pages/Cart';
+function App(){
   return (
     <div>
+      <BrowserRouter>
      <Navbar/>
+     <Routes>
+      <Route path='/' element={<Shop/>}/>
+      <Route path='/mens' element={<ShopCategory category="men"/>} />
+      <Route path='/womens' element={<ShopCategory category="women" />} />
+      <Route path='/kids' element={<ShopCategory category="kid" />} />
+      <Route path='/product' element={<Product/>}>
+      <Route  path=':productId' element={<Product/>} />
+      </Route>
+      <Route path='/cart' element={<Cart/>} />
+      <Route path='/Login' element={<LoginSignup/>} />
+     </Routes>
+</BrowserRouter>
     </div>
   );
 }
