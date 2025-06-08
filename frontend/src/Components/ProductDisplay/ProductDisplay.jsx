c9a6e5272266170aa46a36f4ca7fa641c8580315
Aayush Prasai <gayatriprasai@gmail.com>
2025-06-08 13:46:27 +0545

    Added Size and description in Product Page

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -25,9 +25,28 @@ export const ProductDisplay = (props) => {
                 <img src={star_icon} alt="" />
                 <img src={star_icon} alt="" />
                 <img src={star_dull_icon} alt="" />
-                <p>(122</p>
+                <p>(122)</p>
             </div>
-            
+            <div className="productdisplay-right-prices">
+                <div className="productdispay-right-price-old">${product.old_price}</div>
+                <div className="productdispay-right-price-new">${product.new_price}</div>
+            </div>
+            <div className="productdisplay-right-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat delectus dolor asperiores distinctio, sint dignissimos aperiam facere optio ex ratione?
+            </div>
+            <div className="productdisplay-right-size">
+                <h1>Select Size</h1>
+                <div className="productdisplay-right-sizes">
+                    <div>S</div>
+                    <div>M</div>
+                    <div>L</div>
+                    <div>XL</div>
+                    <div>XXL</div>
+                </div>
+            </div>
+            <button>ADD TO CART</button>
+            <p className='productdisplay-right-category'> <span>Category: </span> Women,Tshirt, Crop Top</p>
+            <p className='productdisplay-right-category'> <span>Tags: </span> Modern, Latest ,Tshirt</p>
         </div>
     </div>
   )
