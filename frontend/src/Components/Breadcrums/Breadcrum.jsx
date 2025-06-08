706b9d8f9d6381532e1c1ff98ec28dce3d5e48f4
Aayush Prasai <gayatriprasai@gmail.com>
2025-06-08 20:36:07 +0545

    Addded BreadCrum

--- frontend/src/Components/Breadcrums/Breadcrum.jsx
+++ frontend/src/Components/Breadcrums/Breadcrum.jsx
@@ -5,7 +5,7 @@ import arrow_icon from '../Assets/breadcrum_arrow.png'
 export const Breadcrum = (props) => {
     const {product} = props;
   return (
-    <div className='Breadcrum'>
+    <div className='breadcrum'>
         Home <img src={arrow_icon} alt='' /> SHOP <img src={arrow_icon} alt='' />{product.category} <img src={arrow_icon} alt="" />{product.name}
     </div>
   )
--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -11,14 +11,14 @@ export const ProductDisplay = (props) => {
                 <img src={product.image} alt="" />
                 <img src={product.image} alt="" />
                 <img src={product.image} alt="" />
-                <img src={product.image} alt="" />
+                
             </div>
             <div className="product-img">
                 <img className='productdisplay-main-img' src={product.image} alt="" />
             </div>
         </div>
         <div className="productdisplay-right">
-            <h1>{product.name}</h1>
+            <h2>{product.name}</h2>
             <div className="productdisplay-right-star">
                 <img src={star_icon} alt="" />
                 <img src={star_icon} alt="" />
@@ -35,7 +35,7 @@ export const ProductDisplay = (props) => {
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat delectus dolor asperiores distinctio, sint dignissimos aperiam facere optio ex ratione?
             </div>
             <div className="productdisplay-right-size">
-                <h1>Select Size</h1>
+                <h2>Select Size</h2>
                 <div className="productdisplay-right-sizes">
                     <div>S</div>
                     <div>M</div>
